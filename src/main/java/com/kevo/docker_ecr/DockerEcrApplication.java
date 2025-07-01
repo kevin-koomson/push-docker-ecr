34de5d9cbf688c4892f9f268debe340ba0765eee
Kevin Koomson <andrew.koomson@amalitech.com>
2025-07-01 13:44:33 +0000

    setup application

---
+++ src/main/java/com/kevo/docker_ecr/DockerEcrApplication.java
@@ -0,0 +1,13 @@
+package com.kevo.docker_ecr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerEcrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerEcrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevo/docker_ecr/DockerEcrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevo.docker_ecr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerEcrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
